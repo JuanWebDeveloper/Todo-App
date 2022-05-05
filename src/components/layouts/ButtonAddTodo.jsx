f0a42c0d89c4535096fec3bc672222ea45a67616
Juan Vargas <juandeveloper19@gmail.com>
2022-05-05 04:57:28 -0500

    Creation the button to add to-do

---
+++ src/components/layouts/ButtonAddTodo.jsx
@@ -0,0 +1,9 @@
+export const ButtonAddTodo = () => {
+  return (
+    <div className='button-add_todo'>
+      <button className='button'>
+        <i className='fa-solid fa-plus'></i>
+      </button>
+    </div>
+  );
+};
--- src/containers/Layouts.js
+++ src/containers/Layouts.js
@@ -3,11 +3,13 @@ import { Fragment } from 'react';
 // Components
 import { Footer } from '../components/layouts/Footer';
 import { Header } from '../components/layouts/Header';
+import { ButtonAddTodo } from '../components/layouts/ButtonAddTodo';
 
 const Layouts = ({ children }) => {
   return (
     <Fragment>
       <Header />
+      <ButtonAddTodo />
       {children}
       <Footer />
     </Fragment>
